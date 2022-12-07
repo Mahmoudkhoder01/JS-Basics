1b1dea2ef069effba43bda6831fdd164a130b6e6
mahmoudkhoder01 <mahmoudkhodor06@gmail.com>
2022-12-07 22:08:04 +0200

    Completed step 8

--- javascript-basics-01/Step_7.js
+++ javascript-basics-01/Step_7.js
@@ -1,8 +1,3 @@
-var shoeSize = prompt("Your hsoe size please");
-document.getElementById("shoe_size").value = shoeSize;
-var year = prompt("Your year age please:");
-document.getElementById("year").value = year;
-var result = (shoeSize * 2 + 5)*50 - year + 1766;
-document.getElementById("validate").addEventListener("click", function() {
-    alert(result);
-});
+document.querySelector("#name").onblur = function () {
+    alert("thank you for participating!")
+};
