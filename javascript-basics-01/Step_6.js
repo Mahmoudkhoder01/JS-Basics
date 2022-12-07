3527c122dd5b288e90742546c40b5faf87d8b6f4
mahmoudkhoder01 <mahmoudkhodor06@gmail.com>
2022-12-07 15:46:18 +0200

    Completed step 6

--- javascript-basics-01/Step_6.js
+++ javascript-basics-01/Step_6.js
@@ -1,6 +1,8 @@
+var first = prompt("Your first number:")
+document.getElementById("first_number").value = first;
+var second = prompt("Your second number")
+document.getElementById("second_number").value = second;
+var sum = first % second;
 document.getElementById("validate").addEventListener("click", function() {
-    var first = document.getElementById("first_number").value;
-    var second = document.getElementById("second_number").value;
-    var sum = first % second;
     alert(sum);
-});
+}); 
