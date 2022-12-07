7331ed3c6e89ee569eaa963ff9bf2fa3fbce50c2
mahmoudkhoder01 <mahmoudkhodor06@gmail.com>
2022-12-07 20:30:11 +0200

    Completed step 4

---
+++ javascript-basics-02/step 4/Step_4.js
@@ -0,0 +1,5 @@
+document.querySelector("[name='button']").onclick = function(){
+    document.getElementById("name").value = "";
+    document.getElementById("surname").value = "";
+    document.getElementById("city").value = "";
+}
