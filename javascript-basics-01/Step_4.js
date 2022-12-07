cfb0b58cefd7a4ac86a68b64b7a7b589bad41ec1
mahmoudkhoder01 <mahmoudkhodor06@gmail.com>
2022-12-07 11:35:11 +0200

    Completed step4

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,9 @@
+var name = prompt("Your name please:");
+var surname = prompt("Your surname please:");
+var city = prompt("Your city please:");
+
+alert(`
+    Name: ${name}
+    Surname: ${surname}
+    city: ${city}
+`)
