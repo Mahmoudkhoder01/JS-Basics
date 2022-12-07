d3409b443220c7facbabad847815cdcede033bb5
mahmoudkhoder01 <mahmoudkhodor06@gmail.com>
2022-12-07 22:06:40 +0200

    Completed step 4

---
+++ javascript-basics-03/step 4/Step_4.js
@@ -0,0 +1,32 @@
+let pass = prompt("Type your password:");
+document.getElementById("password").value = pass;
+let conf = prompt("Confirm your password:");
+document.getElementById("confirmation").value = conf;
+
+let passInput = document.getElementById("password");
+let confInput = document.getElementById("confirmation");
+
+document.querySelector("[name='button']").onclick = function (e) {
+  e.preventDefault();
+  if (passInput.value == "" && confInput.value == 0) {
+    alert("Please enter a password!");
+  } else if (passInput.value == confInput.value) {
+    alert("Password Confirmed!");
+  } else {
+    alert("It doesn't match!");
+  }
+};
+// let pass = document.getElementById("password");
+// let conf = document.getElementById("confirmation");
+
+// document.querySelector("[name='button']").onclick = function(e){
+//     e.preventDefault();
+//     if(pass.value == '' && conf.value == 0){
+//         alert("Please enter a password!");
+//     } else if(pass.value == conf.value){
+//         alert("Password Confirmed!");
+//     } else {
+//         alert("It doesn't match!")
+//     }
+// };
+
