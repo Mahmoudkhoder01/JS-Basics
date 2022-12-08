c61fd4914ebaa28323a34eae3cc31e4ea056412f
mahmoudkhoder01 <mahmoudkhodor06@gmail.com>
2022-12-08 09:17:31 +0200

    Completed step4

--- javascript-basics-02/step 4/Step_4.js
+++ javascript-basics-02/step 4/Step_4.js
@@ -1,5 +1,12 @@
-document.querySelector("[name='button']").onclick = function(){
+document.querySelector("[name='button']").onclick = function () {
+  let yesOrno = prompt(
+    "Do you need to delete this? Type yes if you need and no if you don't"
+  );
+  if (yesOrno == "yes") {
     document.getElementById("name").value = "";
     document.getElementById("surname").value = "";
     document.getElementById("city").value = "";
-}
+  } else if (yesOrno == "no") {
+    alert("It will not erased, you are lucky")
+  }
+};
