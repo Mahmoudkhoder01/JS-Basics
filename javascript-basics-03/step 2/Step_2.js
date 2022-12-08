83bd8ad4ac4fb98b437ea03b1ad3d51784e982fb
mahmoudkhoder01 <mahmoudkhodor06@gmail.com>
2022-12-08 12:58:27 +0200

    Completed step2

--- javascript-basics-03/step 2/Step_2.js
+++ javascript-basics-03/step 2/Step_2.js
@@ -1,11 +1,13 @@
 var x = document.getElementById("show");
 var y = document.getElementById("hide");
 var z = document.getElementById("texte")
+var as = document.querySelectorAll("a")
 
-x.onclick = function(){
-    z.style.display = "block";
-}
-
-y.onclick = function(){
-    z.style.display = "none";
-}
+as.forEach((e) => {
+    e.addEventListener("click", (e) =>{
+    if(e.target.id == "hide"){
+        z.style.display = "none"
+    } else {
+        z.style.display = "block"
+    }
+})});
